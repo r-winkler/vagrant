279980f4b92d02910a4acd01db88b84284df328e
René Winkler <renewinkler@gmx.ch>
2016-10-26 11:21:31 +0200

    hubot

---
+++ hubot/Readme.md
@@ -0,0 +1,9 @@
+# General
+
+This will setup a VM with default Hubot without any further configuration.
+
+# Setup
+
+* vagrant up
+* vagrant ssh (pw: vagrant)
+* vagrant ping (should output pong)
--- hubot/provision.sh
+++ hubot/provision.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
 
-apt-get -y update
+# script is re-runnable via "vagrant provision" as it checks if software is already installed
 
-# only install npm if it is not yet installed
+apt-get -y update
+apt-get install curl
+
+# only install npm if it is not yet installed (works like this for debian packages)
 dpkg -s npm &>/dev/null || {
-	apt-get -y install nodejs npm
+	# install nodejs v5
+	# http://stackoverflow.com/questions/34974535/install-latest-nodejs-version-in-ubuntu-14-04
+	curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
+	apt-get install -y nodejs
+	# update npm
+	npm install npm@latest -g 
 	ln -s /usr/bin/nodejs /usr/bin/node
+}
+
+# only install hubot if it is not yet installed
+command -v hubot &>/dev/null || {
+	npm install -g hubot-generator yo coffee-script
+	mkdir myhubot
+	cd myhubot
+	yo hubot --defaults
+	
+	# custom configuration
+	npm uninstall hubot-heroku-keepalive --save
+	sed -i '/hubot-redis-brain/ d' external-scripts.json
+	sed -i '/hubot-heroku-keepalive/ d' external-scripts.json
+	
+	bin/hubot
 }
